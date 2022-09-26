476c5266b3474605d7e5347ab29e4ac4c3b1db5b
kimikimy08 <104545752+kimikimy08@users.noreply.github.com>
2022-09-26 13:07:34 +0800

    settings static

--- signosTrapiko/settings.py
+++ signosTrapiko/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR, 'media')
