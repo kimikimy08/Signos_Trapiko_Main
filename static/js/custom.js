071a98965d915583291ea176fb97bea22d57906e
kimikimy08 <104545752+kimikimy08@users.noreply.github.com>
2022-10-09 21:05:17 +0800

    fix location

--- static/js/custom.js
+++ static/js/custom.js
@@ -19,89 +19,89 @@
 // autocomplete.addListener('place_changed', onPlaceChanged);
 // }
 
-function onPlaceChanged (){
+// function onPlaceChanged (){
     
-    var place = autocomplete.getPlace();
+//     var place = autocomplete.getPlace();
     
-    // User did not select the prediction. Reset the input field or alert()
-    if (!place.geometry){
-        document.getElementById('id_information-address').placeholder = "Start typing...";
-    }
-    else{
-        // console.log('place name=>', place.name)
-    }
+//     // User did not select the prediction. Reset the input field or alert()
+//     if (!place.geometry){
+//         document.getElementById('id_information-address').placeholder = "Start typing...";
+//     }
+//     else{
+//         // console.log('place name=>', place.name)
+//     }
 
-    // get the address components and assign them to the fields
-    // console.log(place);
-    var geocoder = new google.maps.Geocoder()
-    var address = document.getElementById('id_information-address').value
+//     // get the address components and assign them to the fields
+//     // console.log(place);
+//     var geocoder = new google.maps.Geocoder()
+//     var address = document.getElementById('id_information-address').value
 
-    geocoder.geocode({'address': address}, function(results, status){
-        // console.log('results=>', results)
-        // console.log('status=>', status)
-        if(status == google.maps.GeocoderStatus.OK){
-            var latitude = results[0].geometry.location.lat();
-            var longitude = results[0].geometry.location.lng();
+//     geocoder.geocode({'address': address}, function(results, status){
+//         // console.log('results=>', results)
+//         // console.log('status=>', status)
+//         if(status == google.maps.GeocoderStatus.OK){
+//             var latitude = results[0].geometry.location.lat();
+//             var longitude = results[0].geometry.location.lng();
 
-            // console.log('lat=>', latitude);
-            // console.log('long=>', longitude);
-            $('#id_information-latitude').val(latitude);
-            $('#id_information-longitude').val(longitude);
+//             // console.log('lat=>', latitude);
+//             // console.log('long=>', longitude);
+//             $('#id_information-latitude').val(latitude);
+//             $('#id_information-longitude').val(longitude);
 
-            $('#id_information-address').val(address);
-        }
-    });
+//             $('#id_information-address').val(address);
+//         }
+//     });
 
-    // loop through the address components and assign other address data
-    console.log(place.address_components);
-    for(var i=0; i<place.address_components.length; i++){
-        for(var j=0; j<place.address_components[i].types.length; j++){
-            // get country
-            if(place.address_components[i].types[j] == 'country'){
-                $('#id_information-country').val(place.address_components[i].long_name);
-            }
-            // get state
-            if(place.address_components[i].types[j] == 'administrative_area_level_1'){
-                $('#id_information-state').val(place.address_components[i].long_name);
-            }
-            // get city
-            if(place.address_components[i].types[j] == 'locality'){
-                $('#id_information-city').val(place.address_components[i].long_name);
-            }
-            // get pincode
-            if(place.address_components[i].types[j] == 'postal_code'){
-                $('#id_information-pin_code').val(place.address_components[i].long_name);
-            }else{
-                $('#id_information-pin_code').val("");
-            }
-        }
-    }
+//     // loop through the address components and assign other address data
+//     console.log(place.address_components);
+//     for(var i=0; i<place.address_components.length; i++){
+//         for(var j=0; j<place.address_components[i].types.length; j++){
+//             // get country
+//             if(place.address_components[i].types[j] == 'country'){
+//                 $('#id_information-country').val(place.address_components[i].long_name);
+//             }
+//             // get state
+//             if(place.address_components[i].types[j] == 'administrative_area_level_1'){
+//                 $('#id_information-state').val(place.address_components[i].long_name);
+//             }
+//             // get city
+//             if(place.address_components[i].types[j] == 'locality'){
+//                 $('#id_information-city').val(place.address_components[i].long_name);
+//             }
+//             // get pincode
+//             if(place.address_components[i].types[j] == 'postal_code'){
+//                 $('#id_information-pin_code').val(place.address_components[i].long_name);
+//             }else{
+//                 $('#id_information-pin_code').val("");
+//             }
+//         }
+//     }
 
-}
+// }
 
-var input = document.getElementById('id_information-address');
-var options = {
-    componentRestrictions: {
-        country: 'ph'
-    }
-};
+// var input = document.getElementById('id_information-address');
+// var options = {
+//     componentRestrictions: {
+//         country: 'ph'
+//     }
+// };
 
-var autocomplete = new google.maps.places.Autocomplete(input, options);
+// var autocomplete = new google.maps.places.Autocomplete(input, options);
 
-$(input).on('input',function(){
-	var str = input.value;
-  var prefix = 'Quezon City, ';
-	if(str.indexOf(prefix) == 0) {
-		console.log(input.value);
-	} else {
-		if (prefix.indexOf(str) >= 0) {
-    	input.value = prefix;
-    } else {
-  		input.value = prefix+str;
-   }
-	}
+// $(input).on('input',function(){
+// 	var str = input.value;
+//   var prefix = 'Quezon City, ';
+// 	if(str.indexOf(prefix) == 0) {
+// 		console.log(input.value);
+// 	} else {
+// 		if (prefix.indexOf(str) >= 0) {
+//     	input.value = prefix;
+//     } else {
+//   		input.value = prefix+str;
+//    }
+// 	}
 
-});
+// });
 
 google.maps.event.addListener(autocomplete, 'place_changed', function () {
     
@@ -135,6 +135,31 @@ google.maps.event.addListener(autocomplete, 'place_changed', function () {
             $('#id_information-address').val(address);
         }
     })
+
+    console.log(place.address_components);
+    for(var i=0; i<place.address_components.length; i++){
+        for(var j=0; j<place.address_components[i].types.length; j++){
+            // get country
+            if(place.address_components[i].types[j] == 'country'){
+                $('#id_information-country').val(place.address_components[i].long_name);
+            }
+            // get state
+            if(place.address_components[i].types[j] == 'administrative_area_level_1'){
+                $('#id_information-state').val(place.address_components[i].long_name);
+            }
+            // get city
+            if(place.address_components[i].types[j] == 'locality'){
+                $('#id_information-city').val(place.address_components[i].long_name);
+            }
+            // get pincode
+            if(place.address_components[i].types[j] == 'postal_code'){
+                $('#id_information-pin_code').val(place.address_components[i].long_name);
+            }else{
+                $('#id_information-pin_code').val("");
+            }
+        }
+    }
+
 });
 
 
