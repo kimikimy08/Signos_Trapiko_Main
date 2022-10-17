2db15024643ba9662b7dfc3fe70f9011f7ed17cf
kimikimy08 <104545752+kimikimy08@users.noreply.github.com>
2022-10-17 18:34:24 +0800

    incident

--- signosTrapiko/settings.py
+++ signosTrapiko/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = Path(BASE_DIR, "templates")
 
 # remove
-STATIC_DIR = Path(BASE_DIR, "static")
+# STATIC_DIR = Path(BASE_DIR, "static")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
