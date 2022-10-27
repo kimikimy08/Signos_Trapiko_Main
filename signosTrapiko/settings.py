0269fd28407f00d6feb823530e2d974cc1cf45d7
kimikimy08 <104545752+kimikimy08@users.noreply.github.com>
2022-10-27 11:18:56 +0800

    revised registration page

--- signosTrapiko/settings.py
+++ signosTrapiko/settings.py
@@ -168,8 +168,8 @@ DATE_INPUT_FORMATS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # remove
 # STATIC_URL = 'static/'
