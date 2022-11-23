579a0ba2241066a1b0949ee8da41c06fa20fab40
kimikimy08 <104545752+kimikimy08@users.noreply.github.com>
2022-11-24 03:27:59 +0800

    first commit

--- signosTrapiko/settings.py
+++ signosTrapiko/settings.py
@@ -21,8 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = Path(BASE_DIR, "templates")
 
 # remove
-# STATIC_DIR = Path(BASE_DIR, "static")
+STATIC_DIR = Path(BASE_DIR, "static")
 
+
+#for media store in bucket
+# getting credentials
 from google.oauth2 import service_account
 GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
     os.path.join(BASE_DIR, 'credentials.json')
@@ -35,7 +38,6 @@ GS_BUCKET_NAME = 'signos_trapiko'
 MEDIA_ROOT = "media/"
 UPLOAD_ROOT = 'media/uploads/'
 MEDIA_URL = 'https://storage.googleapis.com/{}/'.format(GS_BUCKET_NAME)
-
 
 
 # Quick-start development settings - unsuitable for production
@@ -60,6 +62,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'formtools',
+    'ckeditor',
     'django.contrib.gis',
     'django.contrib.humanize',
     
@@ -71,7 +74,7 @@ INSTALLED_APPS = [
     'generate_report',
     'dashboard',
     'inbox',
-    'notifications'
+    'notifications',
     # 'livereload'
     
 ]
@@ -103,6 +106,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_google_api',
+                'inbox.views.check_inbox'
 
             ],
         },
@@ -152,7 +156,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -178,19 +181,18 @@ DATE_INPUT_FORMATS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # remove
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     STATIC_DIR,
-# ]
-# STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = Path(BASE_DIR, 'media')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -211,19 +213,18 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Link Technologies <linktechnologies2022@gmail.com>'
 
 # whitenoise settings
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 GOOGLE_API_KEY = config('GOOGLE_API_KEY')
 
-# GDAL_LIBRARY_PATH = "/opt/homebrew/Cellar/gdal/3.5.2_1/lib/libgdal.dylib"
-# GEOS_LIBRARY_PATH = "/opt/homebrew/Cellar/geos/3.11.0/lib/libgeos_c.dylib"
+GDAL_LIBRARY_PATH = "/opt/homebrew/Cellar/gdal/3.5.2_1/lib/libgdal.dylib"
+GEOS_LIBRARY_PATH = "/opt/homebrew/Cellar/geos/3.11.0/lib/libgeos_c.dylib"
 
-GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+# GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+# GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 
-DATABASES['default'] = dj_database_url.config()
-DATABASES['default']['ENGINE'] = "django.contrib.gis.db.backends.postgis"
-
+# DATABASES['default'] = dj_database_url.config()
+# DATABASES['default']['ENGINE'] = "django.contrib.gis.db.backends.postgis"
 
 
 
